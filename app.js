4964a3aad2317af0c086a4a9916d02682c8d8b52
Robbie Bardijn <robbie.bardijn@gmail.com>
2013-05-26 10:43:57 +0200

    initialize

---
+++ app.js
@@ -0,0 +1,43 @@
+(function() {
+  var app, express, http, path, routes, user;
+
+  express = require('express');
+
+  routes = require('./routes');
+
+  user = require('./routes/user');
+
+  http = require('http');
+
+  path = require('path');
+
+  app = express();
+
+  app.configure(function() {
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express["static"](path.join(__dirname, 'assets')));
+    return this;
+  });
+
+  app.configure('development', function() {
+    app.use(express.errorHandler());
+    return this;
+  });
+
+  app.get('/', routes.index);
+
+  app.get('/users', user.list);
+
+  http.createServer(app).listen(app.get('port'), function() {
+    console.log("Express server listening on port " + app.get('port'));
+    return this;
+  });
+
+}).call(this);
